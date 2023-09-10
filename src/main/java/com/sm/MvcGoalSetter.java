e0dd1b602909db029d36b4eb84e77ebdbf3e8833
msalakhedenov <m.salakhedenov@gmail.com>
2023-09-10 17:41:09 +0600

    fix: make application deployable as WAR

--- src/main/java/com/sm/MvcGoalSetter.java
+++ src/main/java/com/sm/MvcGoalSetter.java
@@ -2,9 +2,10 @@ package com.sm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MvcGoalSetter {
+public class MvcGoalSetter extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(MvcGoalSetter.class);
